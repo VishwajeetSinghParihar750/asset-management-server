a771659d6eaa0768a7fceb296b5d867761410e56
VishwajeetSinghParihar750 <sVishwajeet750@gmail.com>
2022-12-06 01:59:17 +0530

    vercel test2

--- routes/register.js
+++ routes/register.js
@@ -15,31 +15,36 @@ router.get("/register", (req, res) => {
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
-  // checkign if user exists
-  const userWithEmail = await User.findOne({ email: email }).catch((e) =>
-    console.log(e)
-  );
+  try {
+    // checkign if user exists
+    const userWithEmail = await User.findOne({ email: email }).catch((e) =>
+      console.log(e)
+    );
 
-  if (userWithEmail) {
-    return res.status(400).json({ message: "User already Exists !" });
+    if (userWithEmail) {
+      return res.status(400).json({ message: "User already Exists !" });
+    }
+
+    // saving user
+    let hashedPassword = await hashPassword(password);
+
+    let newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    // sending response
+    const jwtToken = jwt.sign(
+      {
+        id: newUser._id,
+        email: newUser.email,
+      },
+      process.env.JWT_SECRET_KEY
+    );
+
+    res.json({ message: "Welcome !", token: jwtToken });
+  } catch (e) {
+    res.status(400).json({ error: e });
+    console.log(e);
   }
-
-  // saving user
-  let hashedPassword = await hashPassword(password);
-
-  let newUser = new User({ name, email, password: hashedPassword });
-  await newUser.save();
-
-  // sending response
-  const jwtToken = jwt.sign(
-    {
-      id: newUser._id,
-      email: newUser.email,
-    },
-    process.env.JWT_SECRET_KEY
-  );
-
-  res.json({ message: "Welcome !", token: jwtToken });
 });
 
 module.exports = router;
